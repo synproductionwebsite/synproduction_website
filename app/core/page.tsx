908ae4fccc4209ff56166e6af6e7db9baa8966ca
pelleservan <pelle.servan@hotmail.fr>
2025-07-08 22:29:27 +0200

    add test

--- app/core/page.tsx
+++ app/core/page.tsx
@@ -5,11 +5,10 @@ import React from "react";
 const CorePage = () => {
   return (
     <>
-    <h1>DEV</h1>
     <div className="text-[#1F1F1F] max-w-5xl mx-auto p-6">
       <div className="">
         <p className="bg-customPurple1 rounded-4xl font-mono text-justify">
-           POPA l&apos;origine de SYN Production, une envie partagée : explorer les silences dans la partition de nos vies prestissimo. Comment renouveler notre rapport à la Culture ? 
+           L&apos;origine de SYN Production, une envie partagée : explorer les silences dans la partition de nos vies prestissimo. Comment renouveler notre rapport à la Culture ? 
            Comment redécouvrir notre rapport aux autres et à soi ? Comment prendre de nouveau le temps ?
            Dans un monde d&apos;accélération et de quête de sens, nous souhaitons porter un projet où l&apos;on laisse place à l&apos;échange, à la création et à la réflexion.
            C&apos;est en créant des espaces de revalorisation de lien social et en favorisant l&apos;émergence et la diffusion du Beau que l&apos;on souhaite donner corps à SYN Production.
